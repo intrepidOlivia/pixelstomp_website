cfb825b1cbc8d54ba2b37e7dfc58837b3190d5a3
Marisha Parker <m_parker2@wargaming.net>
2019-09-20 08:15:41 -0700

    First commit of reddit lookup files

---
+++ reddit_lookup_utility/wordcloud_module.js
@@ -0,0 +1,8 @@
+﻿exports.getWordCloud = (commentArray) => {
+    //Returns (an image?) the wordcloud to the document to be displayed
+};
+
+//TODO: Find a word cloud processor on github
+
+// A markov chain is a system that can receive textual () input and learn its grammar.
+// It can then generate a state machine that
