6c2d56ba8425290d60e4f0b9e110b469ca4f1db4
Marisha Parker <marishaoptera@fb.com>
2022-12-22 12:47:05 -0800

    Update fic parsing to include header tags

--- apps/fanfic_theater/text_parsing.js
+++ apps/fanfic_theater/text_parsing.js
@@ -6,21 +6,33 @@ function extractParagraphs(text) {
         const matchArray = Array.from(match);
         let pindex = null;
         for (let m of matchArray) {
+            // Handle paragraphs
             if (m[0].includes('<p')) {
                 pindex = m.index + m[0].length; // Set cursor to start of the paragraph's content
             }
             if (m[0].includes('/p>')) {
                 paragraphs.push(text.slice(pindex, m.index));
             }
+
+            // Handle line breaks
             if (m[0].includes('<hr')) {
                 let prev = paragraphs[paragraphs.length - 1];
                 if (prev !== undefined) {
                     paragraphs[paragraphs.length - 1] = prev.concat(m[0]);
                 }
             }
+
+            // Handle headers
+            if (startsAHeader(m[0])) {
+                // Handle <h1>, <h2>, <h3> etc
+                pindex = m.index + m[0].length; // Set cursor to start of header's content
+            }
+            if (endsAHeader(m[0])) {
+                paragraphs.push(text.slice(pindex, m.index));
+            }
         }
 
-        // If there were no <p> tags, try extracting by the StartFragment tag
+        // If there was no paragraph or header content, try extracting by the StartFragment tag
         if (paragraphs.length < 1) {
             for (let m of matchArray) {
                 if (m[0] === "<!--StartFragment-->") {
@@ -41,6 +53,14 @@ function extractParagraphs(text) {
         paragraphs = text.split(WHITESPACE);
     }
     return paragraphs;
+}
+
+function startsAHeader(text) {
+    return text.includes('<h1') || text.includes('<h2') || text.includes('<h3');
+}
+
+function endsAHeader(text) {
+    return text.includes('/h1>') || text.includes('/h2>') || text.includes('/h3>');
 }
 
 function detectHTML(text) {
